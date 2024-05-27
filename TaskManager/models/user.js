c00e92e297f3682eeb782b166f67325f43841b4f
Anand Verma <anandverma552@gmail.com>
2024-05-26 21:58:03 -0400

    adding bcrypt and login feature

--- TaskManager/models/user.js
+++ TaskManager/models/user.js
@@ -1,36 +1,59 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const User = mongoose.model('User', {
-    name : {
-        type: String,
-        required: true,
-        trim : true
-    },
-    email : {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-           if(!validator.isEmail(value)) throw new Error('Email is invalid') 
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('password')) throw new Error('Password cannot contain "password"')
-        }
-    },
-    age: {
-        type: Number,
-        required: true,
-        validate(value){
-            if(value<0) throw new Error('Invalid Age')
-        }
-    }
-})
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require('bcryptjs')
 
-module.exports = User
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error("Email is invalid");
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password"))
+        throw new Error('Password cannot contain "password"');
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) throw new Error("Invalid Age");
+    },
+  },
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    console.log("here")
+  const user = await User.findOne({ email });
+  if(!user) throw new Error('Unable to login')
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if(!isMatch) throw new Error('Unable to login')
+   console.log(user)  
+  return user  
+};
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- TaskManager/routes/user.js
+++ TaskManager/routes/user.js
@@ -1,52 +1,73 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-const userRouter = new express.Router()
+const userRouter = new express.Router();
 
+userRouter.post("/users", async (req, res) => {
+  const { password } = req.body;
 
-userRouter.post("/users", (req, res) => {
-    const user = new User(req.body);
-    user
-      .save()
-      .then(() => {
-        res.status(200).send("user Saved");
-      })
-      .catch((err) => {
-        res.status(400).send(err);
-      });
-  });
-  
-  
-  userRouter.get("/users", async (req, res) => {
-    try {
-      const users = await User.find();
-      if (users) res.status(200).send(users);
-    } catch (err) {
-      res.status(400).send("Couldn't find users")
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const userObj = {
+    ...req.body,
+    password: hashedPassword,
+  };
+
+  const user = new User(userObj);
+  user
+    .save()
+    .then(() => {
+      res.status(200).send("user Saved");
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+userRouter.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users) res.status(200).send(users);
+  } catch (err) {
+    res.status(400).send("Couldn't find users");
+  }
+});
+
+userRouter.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedIUpdates = ["name", "email", "age", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedIUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Operation" });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
     }
-  });
-  
-  userRouter.patch('/users/:id',async(req, res)=>{
-      const updates  = Object.keys(req.body)
-      const allowedIUpdates = ['name', 'email', 'age', 'password']
-      const isValidOperation = updates.every(update=>allowedIUpdates.includes(update))
-  
-      if(!isValidOperation){
-          return res.status(400).send({error: 'Invalid Operation'})
-      }
-      try {
-          const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-              new: true,
-              runValidators: true
-          })
-  
-          if(!user){
-              return res.status(404).send()
-          }
-          res.status(201).send(user)
-      }catch(err){
-          return res.status(404).send(err)
-      }
-  }) 
+    res.status(201).send(user);
+  } catch (err) {
+    return res.status(404).send(err);
+  }
+});
 
-  module.exports = userRouter
+
+userRouter.post('/users/login',async(req, res)=>{
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.status(200).send(user)
+    } catch(err){
+        res.status(400).send()
+    }
+})
+
+
+module.exports = userRouter;
